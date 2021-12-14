5357147508497fbe6223b40726735aca23ab9f81
MindFaktur <69393019+MindFaktur@users.noreply.github.com>
2021-12-14 18:25:03 +0530

    Update employeeWageCalculator.java

--- employeeWageCalculator.java
+++ employeeWageCalculator.java
@@ -1,14 +1,17 @@
 public class employeeWageCalculator{
-
-	public static void main(String[] args){
-		int fullTime=2, partTime=1, wagePerHr=20, partTimeHr=8, fullTimeHr=16;
-		int random_number=(int)(Math.random()*10%3);
-		if (random_number==fullTime){
-			System.out.println("Employee has worked full time so Daily wage is " + (wagePerHr*fullTimeHr));
-		}else if (random_number==partTime){
-			System.out.println("Employee has worked part time so Daily wage is " + (wagePerHr*partTimeHr));
-		}else{
-			System.out.println("Employee was absent so wage is 0");
-		}
-
+   public static void main(String[] args){
+	final int fullTime = 2, partTime = 1, wagePerHr = 20, partTimeHr = 8, fullTimeHr = 16;
+	int random_number=(int)(Math.random() * 10 % 3);
+	switch (random_number){
+		case fullTime:
+				System.out.println("Employee has worked  full time  so Daily wage is " + (wagePerHr * fullTimeHr));
+							break;
+		case partTime:
+				System.out.println("Employee has worked part time so Daily wage is " + (wagePerHr * partTimeHr));
+							break;
+		default:
+				System.out.println("Employee was absent so wage is 0");
 	}
+
+    }
+}
